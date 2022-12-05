0b32dfa6eeb39aab463da02c972a57ae94242e3f
monktar <monktar@gmail.com>
2022-12-05 12:38:30 -0600

    Update assign_using_if_and_strings.sh

--- assign_using_if_and_strings.sh
+++ assign_using_if_and_strings.sh
@@ -27,3 +27,18 @@ signa_type=
                 echo "$signa_type and $t"
                 # rm $signa # because the user doen't the right to write file to root on destination directory
                 #signa=$(find .  -type f -name "ready_for_short_term_*.txt" | head -n 1)
+
+
+#https://linuxize.com/post/bash-concatenate-strings/
+db='TestDB'
+cd /mnt/sqlbackup/sqlprimary/ASP-SQL/MedRx/LOGSHIPPING
+echo "${db}_*.trn"
+echo $db'_*.trn'
+
+echo "#raw name output"
+find -name 'TestDB*.trn' -mtime +1 -exec ls -lth "{}" \; | wc -l
+echo "#name cancatenate with literal string variable interpolation output"
+find -name "${db}_*.trn" -mtime +1 -exec ls -lth "{}" \; | wc -l
+echo "#name from string1string2 output "
+find -name $db'_*.trn' -mtime +1 -exec ls -lth "{}" \; | wc -l
+~
