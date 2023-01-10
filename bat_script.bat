30e3456b70606931bdffa939c5c73b55fc76f2f2
monktar <monktar@gmail.com>
2023-01-10 15:21:23 -0600

    Update bat_script.bat

--- bat_script.bat
+++ bat_script.bat
@@ -12,6 +12,7 @@ REM echo %MY_STATUS% %date% %time%
 
 PAUSE
 
+REM https://linuxize.com/post/how-to-mount-cifs-windows-share-on-linux/
 REM mount a share folder
 REM bat
  NET USE E: \\sql-backups.rms-asp.com\backup /u:domain\user_name my_password /persistent:yes
