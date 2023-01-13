0543f58c1f157cdadeb2a68198a655086ef3920e
monktar <monktar@gmail.com>
2023-01-13 11:21:26 -0600

    Update bat_script.bat

--- bat_script.bat
+++ bat_script.bat
@@ -16,7 +16,13 @@ REM https://linuxize.com/post/how-to-mount-cifs-windows-share-on-linux/
 REM mount a share folder
 REM bat
  NET USE E: \\sql-backups.rms-asp.com\backup /u:domain\user_name my_password /persistent:yes
- REM NET USE X: /delete
+ REM NET USE E: /delete
 REM bash, ubuntu on temp mounted
 sudo mount -t cifs -o username=mbello@rms-asp.com //ipaddress/share_nmae /home/mbello/shared/new3
  
+
+REM copy over with exclude list in file
+XCOPY  G:  E:  /EXCLUDE:S:\DIFF_exclusion.txt  /c/d/i/s/e/y >> copy_backup_DIFF.log
+REM exclude list like
+.trn
+FULL
